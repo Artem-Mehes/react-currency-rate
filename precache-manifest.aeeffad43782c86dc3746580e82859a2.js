13f8a6c4dbc6391a4e4d6e78ece1ad65b51e7b59
Artem Meges <artemmeges@gmail.com>
2020-08-27 18:01:29 +0300

    Updates

---
+++ precache-manifest.aeeffad43782c86dc3746580e82859a2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a88f4419fb7ea0a99d15086a361ff16",
+    "url": "/react-currency-rate/index.html"
+  },
+  {
+    "revision": "79eb9bec79226e80d1cf",
+    "url": "/react-currency-rate/static/css/main.72898a8a.chunk.css"
+  },
+  {
+    "revision": "1931a1702b2b70205d2b",
+    "url": "/react-currency-rate/static/js/2.e5412a11.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-currency-rate/static/js/2.e5412a11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79eb9bec79226e80d1cf",
+    "url": "/react-currency-rate/static/js/main.4aee0c99.chunk.js"
+  },
+  {
+    "revision": "fe8d11316d234f2b3f8f",
+    "url": "/react-currency-rate/static/js/runtime-main.8c4e8ed5.js"
+  },
+  {
+    "revision": "5c89f14fbfd06ca88f773a6f870a18dd",
+    "url": "/react-currency-rate/static/media/arrow-left-s-line.5c89f14f.svg"
+  },
+  {
+    "revision": "ba463ccb5045da35d73dcd81c45b1df0",
+    "url": "/react-currency-rate/static/media/arrow-right-s-line.ba463ccb.svg"
+  }
+]);
